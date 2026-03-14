7955105ae319862ced0028f6aa7b63b2d640acec
Matteo Merli <mmerli@apache.org>
2026-03-14 10:25:33 -0700

    Suppress Unsafe removal warnings on Java 21+
    Add @SuppressWarnings("removal") to LightProtoCodec for sun.misc.Unsafe
    deprecation on Java 21+.

--- code-generator/src/main/resources/io/github/merlimat/lightproto/generator/LightProtoCodec.java
+++ code-generator/src/main/resources/io/github/merlimat/lightproto/generator/LightProtoCodec.java
@@ -20,7 +20,7 @@ import io.netty.buffer.ByteBufUtil;
 
 import java.nio.charset.StandardCharsets;
 
-@SuppressWarnings("sunapi")
+@SuppressWarnings({"sunapi", "removal"})
 class LightProtoCodec {
 
     static final sun.misc.Unsafe UNSAFE;
